0be837c321a63a4316f352589fd51de423b64af7
cs151051 <cs151051@uniwa.gr>
2022-01-04 23:33:03 +0200

    Format code.

--- src/main/java/opsw/uci/prj/starter/OpswgolumwebservApplication.java
+++ src/main/java/opsw/uci/prj/starter/OpswgolumwebservApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpswgolumwebservApplication {
+public class OpswgolumwebservApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(OpswgolumwebservApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OpswgolumwebservApplication.class, args);
+    }
 
 }
