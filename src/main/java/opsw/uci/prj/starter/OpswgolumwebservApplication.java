1bcdab4b5c50d9030c20d679351b32849dac5b75
cs151051 <cs151051@uniwa.gr>
2022-01-04 23:20:48 +0200

    Init commit.

---
+++ src/main/java/opsw/uci/prj/starter/OpswgolumwebservApplication.java
@@ -0,0 +1,13 @@
+package opsw.uci.prj.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpswgolumwebservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpswgolumwebservApplication.class, args);
+	}
+
+}
---
+++ src/main/java/opsw/uci/prj/starter/ServletInitializer.java
@@ -0,0 +1,13 @@
+package opsw.uci.prj.starter;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OpswgolumwebservApplication.class);
+	}
+
+}
