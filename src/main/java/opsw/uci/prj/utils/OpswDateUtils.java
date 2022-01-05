5c85fbb3be4566cf6e73f91343eb0d1f3c6b23eb
cs151051 <cs151051@uniwa.gr>
2022-01-05 23:56:23 +0200

    Fixes.

---
+++ src/main/java/opsw/uci/prj/utils/OpswDateUtils.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opsw.uci.prj.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ *
+ * @author oulis
+ */
+public class OpswDateUtils
+{
+  public static String DateToStr01(Calendar calendar)
+  {
+    SimpleDateFormat df = null;
+    String result = "";
+    
+    if (calendar != null)
+    {
+      df = new SimpleDateFormat("dd-mm-yyyy");
+      result = df.format(calendar.getTime());
+    }
+    return result;
+  }
+}
