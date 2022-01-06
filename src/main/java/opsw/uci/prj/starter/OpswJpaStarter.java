a6c691085275d041c90956c2c91d8ee8f4839f23
cs151051 <cs151051@uniwa.gr>
2022-01-06 02:33:18 +0200

    Create server connection pool.

---
+++ src/main/java/opsw/uci/prj/starter/OpswJpaStarter.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opsw.uci.prj.starter;
+
+import javax.activation.DataSource;
+import javax.naming.NamingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jndi.JndiTemplate;
+
+/**
+ *
+ * @author oulis
+ */
+@Configuration
+public class OpswJpaStarter
+{
+
+  @Autowired
+  private Environment env;
+
+  @Bean
+  public DataSource dataSource() throws NamingException
+  {
+    System.out.println("ENV Resource: " + env.getProperty("jdbc.url"));
+    return (DataSource) new JndiTemplate().lookup(env.getProperty("jdbc.url"));
+  }
+}
--- src/main/java/opsw/uci/prj/starter/OpswgolumwebservApplication.java
+++ src/main/java/opsw/uci/prj/starter/OpswgolumwebservApplication.java
@@ -1,20 +1,57 @@
 package opsw.uci.prj.starter;
 
-import opsw.uci.prj.controllers.Gram00Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Για την ρύθμιση των DataSources στον Tomcat πάμε στο conf/server.xml και βάζουμε
+ * στο <GlobalNamingResources/> το εξής:
+ * 
+ * 
+ * <!--https://examples.javacodegeeks.com/enterprise-java/tomcat/tomcat-connection-pool-configuration-example/-->
+  <Resource auth="Container"
+            type="javax.sql.DataSource"
+            name="ORCLH_MINLO"
+            global="ORCLH_MINLO"
+            factory="org.apache.tomcat.jdbc.pool.DataSourceFactory"
+            driverClassName="com.mysql.jdbc.Driver"
+            description="ORCLH MINLO OSS database."
+            url="jdbc:mysql://192.168.1.17:3306/OPSWDB?useSSL=false&amp;useLegacyDatetimeCode=false&amp;serverTimezone=UTC"
+            username="root1"
+            password="rootP"
+            maxTotal="10"
+            maxIdle="10"
+            maxWaitMillis="10000"
+            removeAbandonedTimeout="300"
+            defaultAutoCommit="true"
+  />
+ *
+ * έπειτα πάμε στο config/context.xml και βάζουμε το εξής:
+ * 
+ *  
+ * <ResourceLink name="ORCLH_MINLO"
+               global="ORCLH_MINLO"
+               auth="Container"
+               type="javax.sql.DataSource"
+   />
+ * 
+ * .
+ * @author oulis
+ */
 @SpringBootApplication
 @ComponentScan(
         basePackageClasses =
         {
-          Gram00Controller.class
+          //Gram00Controller.class,
+          OpswJpaStarter.class
         },
         basePackages =
         {
-          "opsw.uci.prj.services"
+          "opsw.uci.prj.services",
+          "opsw.uci.prj.controllers"
         }
 )
 @EnableJpaRepositories(
